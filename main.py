a139a21d2c34f2b00b146c003342080a259a4d1e
片山順平 <katayamajunpei@katayamajunpeinoMacBook-Pro.local>
2023-01-27 23:33:29 +0900

    selenium を削除し requests に置き換え

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import datetime
+import re
 
-from selenium import webdriver
+import requests
+import lxml.html
 
 from enums.where import Where
 from scrapers.site_scraper import SiteScraper
-
-# chromedriver はバックグラウンドで起動
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-driver = webdriver.Chrome('./chromedriver.exe', options=options)  # Optional argument, if not specified will search path.
 
 # ファイル名設定
 now = datetime.datetime.now()
@@ -17,6 +14,7 @@ file = open("./exports/list" + str(now) + ".csv", "w")
 # csv のヘッダ行を入力
 file.write("No,Name,Bundesland,Schultyp,E-Mail,URL,URL(Google)\n")
 
+# csv に No を振るための変数
 no = 0
 site_scraper = SiteScraper()
 # 一応確認したが、40000 までは学校のデータが存在した。
@@ -24,44 +22,62 @@ site_scraper = SiteScraper()
 for i in range(50000):
 	page_no = str(i + 1).zfill(5)
 	url = "http://www.schulliste.eu/schule/" + page_no + "-freie-montessori-schule-mkk/"
-	driver.get(url)
+	
+	# url にリクエストしてレスポンスを取得
+	response = requests.get(url)
+	
+	# ステータスが200番台でない場合次のループへ
+	if response.status_code is not requests.codes.ok:
+		print(f"このページへのアクセスに失敗しました。status_code={response.status_code}")
+		continue
+
+	# レスポンスを xpath で検索可能な html 型に変換する
+	html = lxml.html.fromstring(response.content)
 
 	# 小学校のデータではない場合、次のループへ
-	if site_scraper.is_elemental_school(driver) is False:
+	if site_scraper.is_elemental_school(html) is False:
 		continue
-	
-	# No
-	no += 1
 
 	# Name
 	name_xpath = "//p[@class='map_title red']"
-	name = site_scraper.get_data_by_xpath(driver, name_xpath, Where.TEXT, None)	
+	name = site_scraper.get_data_by_xpath(html, name_xpath, Where.TEXT)	
 	if name == "":
 		# 学校名がそもそも取れない場合、処理を続けても意味がないので次のループへ
 		continue
 
 	# Bundesland
 	state_xpath = "//a[contains(@href, 'bundesland=')]"
-	state = site_scraper.get_data_by_xpath(driver, state_xpath, Where.TEXT, None)
+	state = site_scraper.get_data_by_xpath(html, state_xpath, Where.TEXT)
 
 	# Schultyp
 	classification_xpath = "//a[contains(@href, '?t=')]"
-	classification = site_scraper.get_data_by_xpath(driver, classification_xpath, Where.TEXT, None)
+	classification = site_scraper.get_data_by_xpath(html, classification_xpath, Where.TEXT)
 
 	# E-mail
-	email_xpath = "//a[contains(@title, '@')]"
-	email = site_scraper.get_data_by_xpath(driver, email_xpath, Where.ATTRIBUTE, "title")
+	email_xpath = "//a[contains(@title, '@')]/@title"
+	email = site_scraper.get_data_by_xpath(html, email_xpath, Where.ATTRIBUTE)
 	if email == "":
 		# 例のサイトにデータがない場合、google で検索する
 		formatted_name = name.replace(" ", "+")
 		google_search_url = f'https://www.google.com/search?q={formatted_name}+%22e-mail%22'
-		email = site_scraper.google_search(driver, google_search_url)
+		google_search_response = requests.get(url)
+		# メールアドレスにマッチする正規表現
+		pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[a-zA-Z0-9])");
+		# メールアドレスは、google 検索結果全体から探す
+		result = re.search(pattern, google_search_response.content.decode())
+		if result is None:
+			# 検索結果にメールアドレスの記載がない場合
+			email = ""
+		email = result.group()
 	else:
 		# google 検索しない場合は空
+		# このカラムにデータがある場合、メールアドレスが正確か疑った方が良い
 		google_search_url = ""
+	
+	# このデータに採番される No
+	no += 1
 
 	# ファイル書き込み
 	file.write(f'{str(no)},"{name}","{state}","{classification}","{email}","{url}","{google_search_url}"' + "\n")
 
-driver.quit()
 file.close()
--- scrapers/site_scraper.py
+++ scrapers/site_scraper.py
@@ -1,72 +1,44 @@
-import re
-
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 from enums.where import Where
 
 class SiteScraper:
 
     @staticmethod
-    def is_elemental_school(driver):
+    def is_elemental_school(html):
         """渡されたページ情報から、そのページが小学校のものかどうかを判定する
         
         Keyword arguments:
-        driver -- celenium の webdriver
+        html -- requests で取得した html
         """
         try:
-            elements = driver.find_elements(By.XPATH, "//a[contains(@title, 'Zu Schulen im Fachbereich übergehen')]")
+            elements = html.xpath("//a[contains(@title, 'Zu Schulen im Fachbereich übergehen')]")
             for element in elements:
                 # 取得した要素に Grund が含まれる文字列があれば true
                 if "Grund" in element.text:
                     return True
-        except NoSuchElementException:
+        except IndexError:
             # そもそもカテゴリが見つからない場合
             return False
         return False
         
 
     @staticmethod
-    def get_data_by_xpath(driver, xpath, where, attribute_name):
+    def get_data_by_xpath(html, xpath, where):
         """データを xpath を用いて取得し、返却する
         
         Keyword arguments:
-        driver -- celenium の webdriver
+        html -- requests で取得した html
         xpath -- データがあると思われる xpath
         where -- xpath で取得したデータのどの部分が欲しいか
                 ex: text -> Where.TEXT
-        attribute_name -- Where.ATTRIBUTE が指定されている場合、どの attribute から取得するか
-                ex: href の中身が欲しい -> href
         """
         try:
-            element = driver.find_element(By.XPATH, xpath)
-        except NoSuchElementException:
-            # データが見つからなかったら空
+            element = html.xpath(xpath)[0]
+        except IndexError:
             return ""
         if where is Where.TEXT:
             return element.text
         if where is Where.ATTRIBUTE:
-            return element.get_attribute(attribute_name)
+            # attribute から取得するとき、element は配列ではない
+            return element
         # where の指定が想定外の場合も空文字を返却
         return ""
-
-    @staticmethod
-    def google_search(driver, url):
-        """学校名 + e-mail で google 検索し、検索結果からメールアドレスを探して返却する
-
-        driver -- celenium の webdriver
-        url -- 検索用 URL
-        """
-        driver.get(url)
-        try:
-            # 取得範囲は検索結果の全体
-            element = driver.find_element(By.XPATH, "//div[@class='GyAeWb']").text
-        except NoSuchElementException:
-            # 検索結果が1件もない場合
-            return ""
-        # hogehoge@test.com. のように末尾に dot がつく場合がそこそこあるので末尾は必ず英数字とする正規表現
-        pattern = re.compile(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[a-zA-Z0-9])");
-        result = re.search(pattern, element)
-        if result is None:
-            # 検索結果の1件目にメールアドレスの記載がない場合
-            return ""
-        return result.group()
